2d0fd6613f9533400beb491ce4dffd1909018875
LeeJuHee <veryveryhappy@imcloud.co.kr>
2022-07-15 14:27:44 +0900

    loadingCircle

---
+++ src/components/loading/loadingCircle/loadingCircle.js
@@ -0,0 +1,13 @@
+import './loadingCircle.css'
+
+export default function LoadingCircle() {
+    return (
+        <>
+        <div class="loading">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import LoadingCircle from './components/loading/loadingCircle/loadingCircle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+<>
+<LoadingCircle></LoadingCircle>
+</>
+);

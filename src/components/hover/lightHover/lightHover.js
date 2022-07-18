0fa1f94ab87842238b22267b0f2c92a13da6dd70
LeeJuHee <veryveryhappy@imcloud.co.kr>
2022-07-18 16:51:39 +0900

    hover

---
+++ src/components/hover/lightHover/lightHover.js
@@ -0,0 +1,12 @@
+import './light'
+export default function LightHover() {
+  return (
+    <>
+    <span className="left">HELLO</span>
+    <span className="right">HELLO</span>
+    <span className="top">HELLO</span>
+    <span className="bottom">HELLO</span>
+    <span className="center">HELLO</span>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import HamBtn from './components/checked/hamburgerBtn/hamburgerBtn';
+import LightHover from './components/hover/lightHover/lightHover';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-      <HamBtn></HamBtn>
+      <LightHover></LightHover>
     </>
 );

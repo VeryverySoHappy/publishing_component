0957fdf24afd5c3be19a1f7e98dedcc40abb87e1
LeeJuHee <veryveryhappy@imcloud.co.kr>
2022-07-20 16:21:31 +0900

    ipin 폴더이동, gallery

--- src/components/click/ipin/ipin.js
+++ src/components/click/ipin/ipin.js
@@ -1,5 +1,21 @@
 import './ipin.css'
 
+function showIpin() {
+  var txt ='아이핀 인증으로 실명확인을 진행하려면 아래<span>"아이핀 인증받기"</span>를 클릭해 주세요.';
+  document.getElementById('txtContainer').innerHTML=txt;
+  var btn = '<a href="">아이핀 인증받기</a>'
+  document.getElementById('btnContainer').innerHTML=btn;
+}
+
+function showCellPhone() {
+  var txt = '소지하고 계신 핸드폰 번호를 입력해서 인증받는 방법입니다.<br/>';
+  txt+='아래<span>"휴대폰 인증받기"</span>를 클릭해 주세요';
+  document.getElementById('txtContainer').innerHTML=txt;
+  var btn = '<a href="">휴대폰 인증받기</a>'
+  document.getElementById('btnContainer').innerHTML=btn;
+}
+
+
 export default function Ipin() {
   return (
     <>
